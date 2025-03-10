6ca9df4bbf9ceccfee6b697c332b0a81513b43bf
ManGia44 <vienthongphuhung1@gmail.com>
2025-03-10 08:58:23 +0700

    #7 - refactoring about newsfeed-authentic-about pages

---
+++ client/src/app/authentic/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+export default function AuthPage() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace("/authentic/signIn"); // Chuyển hướng về trang đăng nhập
+  }, [router]);
+
+  return null; // Không hiển thị gì cả, chỉ để chuyển hướng
+}
+
+
+
--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -11,6 +11,8 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        primary: "#664545",
+        secondary: '#CDB0A9',
       },
     },
   },
