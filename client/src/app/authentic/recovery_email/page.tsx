551c72c59c1bb53572b1b74a3d4ff9bf61d1cb6a
hoaibao2k4 <hoaibaodongthap@gmail.com>
2025-03-25 21:34:08 +0700

    hot fix v1.2.2

--- client/src/app/authentic/recovery_email/page.tsx
+++ client/src/app/authentic/recovery_email/page.tsx
@@ -1,16 +1,9 @@
-<<<<<<< HEAD
-"use client";
 
-import dynamic from "next/dynamic";
-
-const Recovery_mail = dynamic(() => import("~/components/Forgot Password_tsx/EmailRecovery_tsx/recovery_email"));
-=======
 
 
 import dynamic from "next/dynamic";
 
 const Recovery_mail = dynamic(() => import("~/components/ForgotPassword_tsx/EmailRecovery"));
->>>>>>> dev
 
 export default function Recovery_page (){
     return (
