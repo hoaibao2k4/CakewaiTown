530918c267f3728197bca9932cc9794c7dfabf8e
hoaibao2k4 <hoaibaodongthap@gmail.com>
2025-04-08 21:33:48 +0700

    add google maps

--- client/src/components/Footer/index.jsx
+++ client/src/components/Footer/index.jsx
@@ -11,7 +11,7 @@ import {
 } from "../../../public/assets/icons";
 function Footer() {
   return (
-    <footer className="relative grid grid-custom-4 lg:h-[416px] h-[660px] w-full justify-evenly border-t-2 bg-fifth text-black">
+    <footer className="relative grid grid-custom-4 lg:h-[660px] h-[920px] w-full justify-evenly border-t-2 bg-fifth text-black">
       <div className="lg:col-span-1 col-span-4 items-center lg:py-10 py-4 lg:mx-0 mx-auto">
         <Link href="/">
           <Cakewai className="lg:mt-12 mt-4 mx-auto lg:mx-0" />
@@ -43,7 +43,7 @@ function Footer() {
           </li>
         </ul>
       </div>
-      <div className="lg:my-20 lg:col-span-1 col-span-2 lg:mx-0 mx-4">
+      <div className="lg:mt-20 lg:col-span-1 col-span-2 lg:mx-0 mx-4">
         <h3 className="text-xl font-semibold text-[#664545]">Chính sách</h3>
         <ul className="mt-4 space-y-4 text-base text-[#3D3D3D]">
           <li>
@@ -104,7 +104,16 @@ function Footer() {
           </li>
         </ul>
       </div>
-
+      <div className="col-span-4 mt-1 lg:w-full w-10/12 mx-auto border-t-2 lg:pb-20">
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3918.5050863197785!2d106.76619197485807!3d10.849135689304111!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x317527cc16f6524b%3A0x1fabc1bf1967e238!2sCakewaiTown!5e0!3m2!1sen!2s!4v1744094896953!5m2!1sen!2s"
+          className="w-full rounded-2xl"
+          height={200}
+          allowfullscreen
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+        ></iframe>
+      </div>
       <div className="lg:col-span-1 col-span-4 lg:absolute inset-x-0 bottom-0 mx-auto mt-1 lg:flex lg:w-[60%] lg:items-end items-center justify-center border-t-2 py-4">
         <div className="text-[#B4B9C9]">
           {" "}
