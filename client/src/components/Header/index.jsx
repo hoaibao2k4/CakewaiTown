11f3fb8475d4de9deaccad132c84fdd544af7a60
hoaibao2k4 <hoaibaodongthap@gmail.com>
2025-04-08 08:10:30 +0700

    add gg analytic

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -17,7 +17,7 @@ import { loginSuccess } from "~/redux/authSlice";
 import { setCart } from "~/redux/cartSlice";
 import { updateCartItem } from "~/api/apiCart";
 import { AddToCartContext } from "~/app/modalwapper";
-
+import Script from "next/script";
 function Header() {
   const router = useRouter();
   const dispatch = useDispatch();
@@ -89,113 +89,132 @@ function Header() {
 
   const urls = ["/category", "/birthday", "/tradition", "/cookie", "/bread"];
   return (
-    <header className="fixed top-0 z-[100] h-16 w-full bg-fifth">
-      <div className="mx-10 flex h-full items-center justify-between gap-4 text-primary lg:mx-20">
-        <Link href="/">
-          <Cakewai className="mt-2" />
-        </Link>
-        <ul className="hidden gap-12 px-4 text-base uppercase lg:flex">
-          <li className={pathname === "/" ? "menu-active" : "menu-navbar"}>
-            <Link href="/">TRANG CHỦ</Link>
-          </li>
-          <li
-            className={
-              urls.includes(pathname)
-                ? "menu-active group"
-                : "menu-navbar group"
-            }
-          >
-            <Link href="/category/all" className="relative">
-              <span>MENU BÁNH</span>
-              <i>
-                <ChevronDown className="icon mx-0.5 inline-block size-5" />
-              </i>
-            </Link>
-
-            <div className="absolute left-0 top-full h-4 w-full cursor-default bg-transparent"></div>
-            <ul className="absolute left-0 top-10 z-10 hidden w-max rounded-lg bg-[#CAB6B6] text-sm uppercase group-hover:block">
-              <Link href="/category/birthday">
-                <li className="px-2 py-2 text-[#444444] hover:rounded-t-lg hover:bg-primary hover:text-slate-100">
-                  Bánh Sinh Nhật
-                </li>
+    <div>
+      {/* Script Google Analytics */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-CMNJ5664ZY"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-CMNJ5664ZY');
+        `}
+      </Script>
+      <header className="fixed top-0 z-[100] h-16 w-full bg-fifth">
+        <div className="mx-10 flex h-full items-center justify-between gap-4 text-primary lg:mx-20">
+          <Link href="/">
+            <Cakewai className="mt-2" />
+          </Link>
+          <ul className="hidden gap-12 px-4 text-base uppercase lg:flex">
+            <li className={pathname === "/" ? "menu-active" : "menu-navbar"}>
+              <Link href="/">TRANG CHỦ</Link>
+            </li>
+            <li
+              className={
+                urls.includes(pathname)
+                  ? "menu-active group"
+                  : "menu-navbar group"
+              }
+            >
+              <Link href="/category/all" className="relative">
+                <span>MENU BÁNH</span>
+                <i>
+                  <ChevronDown className="icon mx-0.5 inline-block size-5" />
+                </i>
               </Link>
-              <Link href="/category/tradition">
-                <li className="px-2 py-2 text-[#444444] hover:bg-primary hover:text-slate-100">
-                  Bánh Truyền Thống
-                </li>
-              </Link>
-              <Link href="/category/cookie">
-                <li className="px-2 py-2 text-[#444444] hover:bg-primary hover:text-slate-100">
-                  Cookie & Mini cake
-                </li>
-              </Link>
-              <Link href="/category/bread">
-                <li className="px-2 py-2 text-[#444444] hover:rounded-b-lg hover:bg-primary hover:text-slate-100">
-                  Bánh Mì và Bánh mặn
-                </li>
-              </Link>
-            </ul>
-          </li>
-          <li className={pathname === "/news" ? "menu-active" : "menu-navbar"}>
-            <Link href="/news">TIN TỨC</Link>
-          </li>
-          <li
-            className={
-              pathname === "/generator" ? "menu-active" : "menu-navbar"
-            }
-          >
-            <Link href="/generator">AI</Link>
-          </li>
-          <li className={pathname === "/about" ? "menu-active" : "menu-navbar"}>
-            <Link href="/about">VỀ CHÚNG TÔI</Link>
-          </li>
-        </ul>
 
-        <div className="flex gap-6">
-          {/* Cart Logo */}
+              <div className="absolute left-0 top-full h-4 w-full cursor-default bg-transparent"></div>
+              <ul className="absolute left-0 top-10 z-10 hidden w-max rounded-lg bg-[#CAB6B6] text-sm uppercase group-hover:block">
+                <Link href="/category/birthday">
+                  <li className="px-2 py-2 text-[#444444] hover:rounded-t-lg hover:bg-primary hover:text-slate-100">
+                    Bánh Sinh Nhật
+                  </li>
+                </Link>
+                <Link href="/category/tradition">
+                  <li className="px-2 py-2 text-[#444444] hover:bg-primary hover:text-slate-100">
+                    Bánh Truyền Thống
+                  </li>
+                </Link>
+                <Link href="/category/cookie">
+                  <li className="px-2 py-2 text-[#444444] hover:bg-primary hover:text-slate-100">
+                    Cookie & Mini cake
+                  </li>
+                </Link>
+                <Link href="/category/bread">
+                  <li className="px-2 py-2 text-[#444444] hover:rounded-b-lg hover:bg-primary hover:text-slate-100">
+                    Bánh Mì và Bánh mặn
+                  </li>
+                </Link>
+              </ul>
+            </li>
+            <li
+              className={pathname === "/news" ? "menu-active" : "menu-navbar"}
+            >
+              <Link href="/news">TIN TỨC</Link>
+            </li>
+            <li
+              className={
+                pathname === "/generator" ? "menu-active" : "menu-navbar"
+              }
+            >
+              <Link href="/generator">AI</Link>
+            </li>
+            <li
+              className={pathname === "/about" ? "menu-active" : "menu-navbar"}
+            >
+              <Link href="/about">VỀ CHÚNG TÔI</Link>
+            </li>
+          </ul>
 
-          <div className="relative">
-            <Cart className="navbar-icon" onClick={viewCart} />
-            <span className="absolute bottom-3 left-4 rounded-full bg-fourth px-2.5 text-sm text-slate-100">
-              {list?.length}
-            </span>
-          </div>
+          <div className="flex gap-6">
+            {/* Cart Logo */}
 
-          <Drawer
-            title={
-              <span style={{ fontSize: "20px", color: "#664545" }}>
-                Giỏ hàng
+            <div className="relative">
+              <Cart className="navbar-icon" onClick={viewCart} />
+              <span className="absolute bottom-3 left-4 rounded-full bg-fourth px-2.5 text-sm text-slate-100">
+                {list?.length}
               </span>
-            }
-            onClose={onClose}
-            open={open}
-          >
-            {list?.length > 0 ? (
-              <ListItems list={list} />
+            </div>
+
+            <Drawer
+              title={
+                <span style={{ fontSize: "20px", color: "#664545" }}>
+                  Giỏ hàng
+                </span>
+              }
+              onClose={onClose}
+              open={open}
+            >
+              {list?.length > 0 ? (
+                <ListItems list={list} />
+              ) : (
+                <>
+                  <h1 className="m-6">Thông tin giỏ hàng sẽ xuất hiện ở đây</h1>
+                </>
+              )}
+            </Drawer>
+            {/* User Logo */}
+            {user ? (
+              <UserTooltip
+                onClick={() => setIsLogout(true)}
+                currentUser={user.user}
+              />
             ) : (
-              <>
-                <h1 className="m-6">Thông tin giỏ hàng sẽ xuất hiện ở đây</h1>
-              </>
+              <UserProfile
+                className="navbar-icon"
+                onClick={() => handleLogin()}
+              />
             )}
-          </Drawer>
-          {/* User Logo */}
-          {user ? (
-            <UserTooltip
-              onClick={() => setIsLogout(true)}
-              currentUser={user.user}
-            />
-          ) : (
-            <UserProfile
-              className="navbar-icon"
-              onClick={() => handleLogin()}
-            />
-          )}
-          <div className="lg:hidden">
-            <MobMenu />
+            <div className="lg:hidden">
+              <MobMenu />
+            </div>
           </div>
         </div>
-      </div>
-    </header>
+      </header>
+    </div>
   );
 }
 
